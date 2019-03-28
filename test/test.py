09559e28c0afe0156966ad700067852508feadfb
古丁丁 <s14113103@stu.edu.tw>
2019-03-28 14:33:28 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,7 +1,26 @@
+import os
+import sys
+from argparse import ArgumentParser
+
 from apng import APNG
 
-FILE = 'example/example.png'
+PATH = os.path.abspath(os.path.join(sys.argv[0],os.path.pardir))
 
-apng = APNG.open(FILE)
-apng.num_plays = 0
-apng.save('1.png')
+def APNGplay(FILE, num:int=0, output=None):
+    """set apng play number,0 = loop"""
+    try:
+        apng = APNG.open(FILE)
+        apng.num_plays = 0
+        if not output:
+            output = os.path.splitext(FILE)[0]
+        apng.save(os.path.join(PATH, f'{os.path.basename(output)}_new.png'))
+    except Exception as e:
+        print(f'Error : {str(e)}')
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--input", help="Input APNG file.", dest='input')
+    parser.add_argument("-n", "--num", help="Play number", dest='num')
+    parser.add_argument("-o", "--output", help="output APNG file", dest="output")
+    args = parser.parse_args()
+    APNGplay(args.input, args.num, args.output)
